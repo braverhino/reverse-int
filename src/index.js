e4fccc00e81ff7454afb9edfce98e8552de4126b
braverhino <saparov20003@mail.ru>
2023-01-12 22:48:56 +0600

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let reversed = n.toString().split('').reverse()
+    if (n < 0){
+        reversed.pop()
+        return reversed.join('')
+    }
+  return reversed.join('')
 }
